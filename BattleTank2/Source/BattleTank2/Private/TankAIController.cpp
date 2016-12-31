4e72b5a44df39abf64226666ac8a4b2579aef65b
Steve Barskey <steve@Steves-iMac.local>
2016-12-30 16:47:01 -0800

    After Lecture 118.

--- BattleTank2/Source/BattleTank2/Private/TankAIController.cpp
+++ BattleTank2/Source/BattleTank2/Private/TankAIController.cpp
@@ -7,15 +7,24 @@ void ATankAIController::BeginPlay()
 {
     Super::BeginPlay();
     
-    auto ControlledTank = GetControlledTank();
-    if (!ControlledTank) {
-        UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank."));
+    auto PlayerTank = GetPlayerTank();
+    if (!PlayerTank) {
+        UE_LOG(LogTemp, Warning, TEXT("AIController didn't find Player tank."));
     } else {
-        UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s."), *(ControlledTank->GetName()));
+        UE_LOG(LogTemp, Warning, TEXT("AIController found %s."), *(PlayerTank->GetName()));
     }
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+    auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+    
+    if (!PlayerPawn) { return nullptr; }
+    
+    return Cast<ATank>(PlayerPawn);
 }
