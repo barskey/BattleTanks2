9b1ee7bd4709dc9b0cedfc7704949fb513354dad
Steve Barskey <steve@Steves-iMac.local>
2016-12-31 15:55:11 -0800

    After Lecture 125.

--- BattleTank2/Source/BattleTank2/Private/Tank.cpp
+++ BattleTank2/Source/BattleTank2/Private/Tank.cpp
@@ -10,6 +10,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    // no need to protect pointers as they are added in construction
+    TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -35,6 +37,5 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-    auto MyTankName = GetName();
-    UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *MyTankName, *HitLocation.ToString())
+    TankAimingComponent->AimAt(HitLocation);
 }
