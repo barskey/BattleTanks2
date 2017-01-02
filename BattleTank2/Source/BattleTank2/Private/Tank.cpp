362d06d5dc5841f6a6580a7a1d32f744b8291219
Steve Barskey <steve@Steves-iMac.local>
2017-01-02 13:45:20 -0800

    After Lecture 138.

--- BattleTank2/Source/BattleTank2/Private/Tank.cpp
+++ BattleTank2/Source/BattleTank2/Private/Tank.cpp
@@ -41,3 +41,8 @@ void ATank::AimAt(FVector HitLocation)
 {
     TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
+
+void ATank::Fire()
+{
+    UE_LOG(LogTemp, Warning, TEXT("FIRE!"))
+}
--- BattleTank2/Source/BattleTank2/Public/Tank.h
+++ BattleTank2/Source/BattleTank2/Public/Tank.h
@@ -23,7 +23,10 @@ public:
     
     UFUNCTION(BlueprintCallable, Category=Setup)
     void SetTurretReference(UTankTurret* TurretToSet);
-    
+
+    UFUNCTION(BlueprintCallable, Category=Firing)
+    void Fire();
+
 protected:
     UTankAimingComponent* TankAimingComponent = nullptr;
     
