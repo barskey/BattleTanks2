53b1082af8af63baf2eada69a12a7a2e81a816b2
Steve Barskey <steve@Steves-iMac.local>
2017-01-04 23:04:30 -0800

    After Lecture 152.

--- BattleTank2/Source/BattleTank2/Private/Tank.cpp
+++ BattleTank2/Source/BattleTank2/Private/Tank.cpp
@@ -15,7 +15,6 @@ ATank::ATank()
 
     // no need to protect pointers as they are added in construction
     TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-    TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 // Called when the game starts or when spawned
--- BattleTank2/Source/BattleTank2/Private/TankMovementComponent.cpp
+++ BattleTank2/Source/BattleTank2/Private/TankMovementComponent.cpp
@@ -1,13 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank2.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-    //auto Time = GetWorld()->GetTimeSeconds();
-    UE_LOG(LogTemp, Warning, TEXT("Intend move forward Throw: %f"), Throw)
+    if (!LeftTrack || !RightTrack) { return; }
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+}
 
+void UTankMovementComponent::IntendTurn(float Throw)
+{
+    if (!LeftTrack || !RightTrack) { return; }
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
 }
 
