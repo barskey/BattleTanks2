a5743ec7c1c1903ea9fd01347c24413c74f6bd41
Steve Barskey <steve@Steves-iMac.local>
2017-01-07 20:51:58 -0800

    After Lecture 175.

--- BattleTank2/Source/BattleTank2/Private/TankTrack.cpp
+++ BattleTank2/Source/BattleTank2/Private/TankTrack.cpp
@@ -6,7 +6,7 @@
 
 UTankTrack::UTankTrack()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay() {
@@ -15,27 +15,35 @@ void UTankTrack::BeginPlay() {
     OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+    DriveTrack();
+    ApplyLateralForce();
+    CurrentThrottle = 0;
+}
+
+void UTankTrack::ApplyLateralForce()
 {
     auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+    auto DeltaTime = GetWorld()->GetDeltaSeconds();
     auto AccelCorrection = -SlippageSpeed / DeltaTime * GetRightVector();
     
     // calculate and apply sideways force F=ma
     auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-    auto ForceCorrection = TankRoot->GetMass() * AccelCorrection / 2; // Two tracks
+    auto ForceCorrection = (TankRoot->GetMass() * AccelCorrection) / 2; // Two tracks
 
     TankRoot->AddForce(ForceCorrection);
 }
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+    CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+    auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
-}
-
-void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
-{
-    UE_LOG(LogTemp, Warning, TEXT("OnHit called"))
 }
