824b759f19b9d68634e998f68b9c19063640aa5b
Steve Barskey <steve@Steves-iMac.local>
2017-01-11 21:51:05 -0800

    After Lecture 193.

--- BattleTank2/Source/BattleTank2/TankPlayerController.cpp
+++ BattleTank2/Source/BattleTank2/TankPlayerController.cpp
@@ -89,7 +89,7 @@ bool ATankPlayerController::GetLookDirectionHitLocation( FVector LookDirection, 
      HitResult,
      StartLocation,
      EndLocation,
-     ECollisionChannel::ECC_Visibility)
+     ECollisionChannel::ECC_Camera)
     ) {
         HitLocation = HitResult.Location;
         return true;
